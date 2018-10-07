a9267e0346ba8227c4b589d45845c568469e8fed
JonPotato <43525809+JonPotato@users.noreply.github.com>
2018-10-07 16:47:13 -0700

    Update tkinterMousePosition.py
    Changed function motion to return int
    added canvas

--- tkinterMousePosition.py
+++ tkinterMousePosition.py
@@ -1,9 +1,22 @@
 import tkinter as tk
+from tkinter import *
 root = tk.Tk()
 
+
+#Make canvas with dimensions proportional to belt dimensions
+canvas_width = 745
+canvas_height = 745
+w = Canvas(root, 
+           width=canvas_width,
+           height=canvas_height)
+w.pack()
+posx = 0
+posy = 0
 def motion(event):
     x, y = event.x, event.y
-    print('{}, {}'.format(x, y))
+    posx = x
+    posy = y
+    print('%s , %s' % (posx, posy))
 
 root.bind('<Motion>', motion)
 root.mainloop()
