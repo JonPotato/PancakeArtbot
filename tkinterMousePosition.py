6961f9031e52603d6bb616f3770aceef2c1cb74f
JonPotato <43525809+JonPotato@users.noreply.github.com>
2018-10-07 17:09:54 -0700

    Update tkinterMousePosition.py (#1)
    Output print follows argument syntax for stepper.py
    added direction and steps

--- tkinterMousePosition.py
+++ tkinterMousePosition.py
@@ -3,20 +3,34 @@ from tkinter import *
 root = tk.Tk()
 
 
-#Make canvas with dimensions proportional to belt dimensions
+#Make canvas with dimensions where 
 canvas_width = 745
 canvas_height = 745
 w = Canvas(root, 
            width=canvas_width,
            height=canvas_height)
 w.pack()
-posx = 0
-posy = 0
+currentx = 0
+lastx = 0
+currenty = 0
+lasty = 0
+direction = 'left'
+steps = 0
 def motion(event):
+    global lastx, lasty, currentx, currenty
+    lastx= currentx
+    lasty = currenty
     x, y = event.x, event.y
-    posx = x
-    posy = y
-    print('%s , %s' % (posx, posy))
+    currentx = x
+    currenty = y
+    if currentx <= lastx :
+        direction = 'left'
+        steps = lastx - currentx
+    elif currentx > lastx :
+        direction = 'right'
+        steps = currentx - lastx
+    print('%s %s' %(direction, steps))
+    #print('%s , %s' % (posx, posy))
 
 root.bind('<Motion>', motion)
 root.mainloop()
